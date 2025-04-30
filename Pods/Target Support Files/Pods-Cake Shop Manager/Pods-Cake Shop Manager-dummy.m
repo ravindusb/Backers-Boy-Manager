ecf232189edc71602b7ad9d9a0c0b6671f11630b
meegalaxy <multiplex.galaxy@gmail.com>
2025-04-30 20:55:43 +0300

    first commit

--- Pods/Target Support Files/Pods-Cake Shop Manager/Pods-Cake Shop Manager-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Cake_Shop_Manager : NSObject
-@end
-@implementation PodsDummy_Pods_Cake_Shop_Manager
-@end
--- Pods/Target Support Files/Pods-Cake Shop ManagerUITests/Pods-Cake Shop ManagerUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Cake_Shop_ManagerUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Cake_Shop_ManagerUITests
-@end
